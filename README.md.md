d610faa551bc1fa0c23158d940e7a91d4c7c205b
linzhimin <lzmzhengfu@163.com>
2021-01-06 20:28:52 +0800

    Init

---
+++ README.md.md
@@ -0,0 +1,17 @@
+## AirLine-torch
+This project is a demo of AirLine on torch
+### 1) Convert the fastai model to torch model, and run it, fastai weights([official](https://www.dropbox.com/s/starqc9qd2e1lg1/ArtLine_650.pkl?dl=1) or [baidu](https://pan.baidu.com/s/1O62gmsoSeZcqdaPVUCoHlA) 7v8h)
+
+> python covert2torch.py
+
+### 2) Run the torch model(Converted model [baidu ](https://pan.baidu.com/s/1cUDWhuIjgWrRIlZkWj1mrQ)1kld)
+> python torch_test.py
+
+
+### 3) Enjoy and give a star if you like it.
+
+![image](https://github.com/Linzmin1927/Airline_torch/test.jpg)
+![image](https://github.com/Linzmin1927/Airline_torch/res.jpg)
+
+##reference
+[https://github.com/vijishmadhavan/ArtLine](https://github.com/vijishmadhavan/ArtLine)
---
+++ covert2torch.py
@@ -0,0 +1,43 @@
+import fastai
+from fastai.utils.mem import *
+from fastai.vision import load_learner
+
+import torch.nn as nn
+import torchvision.transforms as T
+from torchsummary import summary
+
+import numpy as np
+import PIL.Image
+import cv2
+
+class FeatureLoss(nn.Module):
+    pass
+
+if __name__ == "__main__":
+    path = Path(".")
+    learn=load_learner(path, 'ArtLine_650.pkl')
+    torch.save(learn.model, "torch.pkl") 
+
+    model = torch.load("torch.pkl")
+    summary(model, (3, 650, 650))
+
+
+    image_path = "test.jpg"
+    img = PIL.Image.open(image_path)
+    img = img.resize((650,650))
+    with torch.no_grad():
+        img_t = T.ToTensor()(img)
+        mean = torch.as_tensor([0.4850, 0.4560, 0.4060])
+        std = torch.as_tensor([0.2290, 0.2240, 0.2250])
+        img_t = (img_t-mean[...,None,None]) / std[...,None,None]
+        img_t = img_t[None]
+        p,img_hr,b = model(img_t)[0]
+        img_hr = img_hr*std[...,None,None] + mean[...,None,None] 
+        img_hr_np = img_hr.to("cpu").numpy()
+        img_hr_np = img_hr_np.transpose((1,2,0))
+
+    fig,ax = plt.subplots(figsize=(9, 9))
+    ax.axis('off')
+    ax.imshow(img_hr_np, 'binary')
+    ax.get_figure().savefig("res.jpg")
+
